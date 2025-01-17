aa77d7e5cac7ff08444989e32a1f9bb6376c23db
Алексей Шелухин <sheluhinalekseypavlovich@gmail.com>
2025-01-17 03:24:53 +0300

    serv: Подключение EF Core

--- server/DAL/ClothingStorePersistence/ApplicationDbContext.cs
+++ server/DAL/ClothingStorePersistence/ApplicationDbContext.cs
@@ -5,6 +5,11 @@ namespace ClothingStorePersistence;
 
 public class ApplicationDbContext : DbContext
 {
-    public DbSet<Cloth> Clothes { get; set; }
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
+    public DbSet<Cloth> Clothes { get; set; } = null!;
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {
+        Database.EnsureCreated();
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder){
+        //modelBuilder.Entity<Cloth>().HasData();
+    }
 }
