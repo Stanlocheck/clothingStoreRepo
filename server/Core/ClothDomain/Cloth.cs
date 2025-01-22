7f22c82dc5a2236090d49a550e3a2803b71bdcd0
Алексей Шелухин <sheluhinalekseypavlovich@gmail.com>
2025-01-22 14:19:34 +0300

    serv: Добавление сущности пользователя и админа магазина

--- server/Core/ClothDomain/Cloth.cs
+++ server/Core/ClothDomain/Cloth.cs
@@ -7,7 +7,7 @@ public class Cloth
     public string? Type { get; set; }
     public string? Brand { get; set; }
     public string? Season { get; set; }
-    public int Size { get; set; }
+    public int? Size { get; set; }
     public string? Material { get; set; }
     public string? Manufacturer { get; set; }
 }
