2352044587f7184693ba40aba5d97a1fb8179486
Алексей Шелухин <sheluhinalekseypavlovich@gmail.com>
2025-01-25 22:27:48 +0300

    serv: Изменены методы контроллеров AdminController и BuyerController, изменен доступ к методам контроллеров

--- server/BLL/ClothesInterfacesBLL/IAdminsBLL.cs
+++ server/BLL/ClothesInterfacesBLL/IAdminsBLL.cs
@@ -5,8 +5,6 @@ namespace ClothesInterfacesBLL;
 
 public interface IAdminsBLL
 {
-    public Task<List<AdminDTO>> GetAll();
-    public Task<AdminDTO> GetById(Guid id);
     public Task AddAdmin(AdminAddDTO admin);
     public Task UpdateAdmin(AdminAddDTO admin, Guid id);
     public Task DeleteAdmin(Guid id);    
